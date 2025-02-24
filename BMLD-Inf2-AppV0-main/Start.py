d7a78952defec9bd092537e8d62f424726570e12
Sara-0215 <cruzssar@students.zhaw.ch>
2025-02-24 10:01:42 +0100

    first commit

---
+++ BMLD-Inf2-AppV0-main/Start.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import pandas as pd
+st.title("Meine erste Streamlit App")
+st.write("Diese App wurde von Anne Müller und Hans Meier entwickelt.")
---
+++ testapp/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Hallo, Streamlit!")
+st.write("Dies ist eine einfache Streamlit-App.")
+
+st.write("Hello World")
+st.button ("Hit me")
+
+
+
