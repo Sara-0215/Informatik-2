2103e86fa3f8d4cb97bf98556d22d1b333622253
Sara-0215 <cruzssar@students.zhaw.ch>
2025-02-26 08:39:44 +0100

    name

--- BMLD-Inf2-AppV0-main/Start.py
+++ BMLD-Inf2-AppV0-main/Start.py
@@ -1,4 +1,16 @@
 import streamlit as st
 import pandas as pd
+
 st.title("Meine erste Streamlit App")
-st.write("Diese App wurde von Anne Müller und Hans Meier entwickelt.")
+
+# !! WICHTIG: Eure Emails müssen in der App erscheinen!!
+
+# Streamlit über den Text unten direkt in die App - cool!
+"""
+Diese App wurde von folgenden Personen entwickelt:
+- Albina Krasniqi (krasnal2@students.zhaw.ch)
+- Sara Cruz Silva (cruzssar@students.zhaw.ch)
+
+Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+
+"""
