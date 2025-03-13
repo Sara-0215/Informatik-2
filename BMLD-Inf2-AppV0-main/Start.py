202b2cfbb0e65c72be511246805a6f36c68566c5
Sara-0215 <cruzssar@students.zhaw.ch>
2025-03-13 15:43:22 +0100

    C2?

--- BMLD-Inf2-AppV0-main/Start.py
+++ BMLD-Inf2-AppV0-main/Start.py
@@ -3,7 +3,16 @@ import pandas as pd
 
 from utils.data_manager import DataManager
 
+# initialize the data manager
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="App")  # switch drive 
 
+# load the data from the persistent storage into the session state
+data_manager.load_app_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(), 
+    parse_dates = ['timestamp']
+)
 #from utils.data_manager import DataManager
 
 st.title("Meine erste Streamlit App")
@@ -19,14 +28,4 @@ Diese App wurde von folgenden Personen entwickelt:
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
 """
-# initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="App")  # switch drive 
-
-# load the data from the persistent storage into the session state
-data_manager.load_app_data(
-    session_state_key='data_df', 
-    file_name='data.csv', 
-    initial_value = pd.DataFrame(), 
-    parse_dates = ['timestamp']
-    )
 
