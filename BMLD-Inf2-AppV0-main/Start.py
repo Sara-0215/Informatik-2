dfe680b42b7cf1665b10d72b54981e31ee284091
Sara-0215 <cruzssar@students.zhaw.ch>
2025-03-19 08:16:59 +0100

    update committed

--- BMLD-Inf2-AppV0-main/Start.py
+++ BMLD-Inf2-AppV0-main/Start.py
@@ -1,20 +1,30 @@
-import streamlit as st
-import pandas as pd
+# ====== Start Init Block ======
+# This needs to copied on top of the entry point of the app (Start.py)
 
+import pandas as pd
 from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
 
 # initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="App")  # switch drive 
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
+
+# initialize the login manager
+login_manager = LoginManager(data_manager)
+login_manager.login_register()  # open login/register page
 
 # load the data from the persistent storage into the session state
-data_manager.load_app_data(
+data_manager.load_user_data(
     session_state_key='data_df', 
     file_name='data.csv', 
     initial_value = pd.DataFrame(), 
     parse_dates = ['timestamp']
-)
-#from utils.data_manager import DataManager
+    )
+# ====== End Init Block ======
 
+# ------------------------------------------------------------
+# Here starts the actual app, which was developed previously
+
+import streamlit as st
 st.title("Meine erste Streamlit App")
 
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
