6115ad57f87105a30a5387525c624c02b6c12432
Albina <krasnal2@students.zhaw.ch>
2025-03-19 19:31:39 +0100

    '''''

--- BMLD-Inf2-AppV0-main/Start.py
+++ BMLD-Inf2-AppV0-main/Start.py
@@ -15,7 +15,7 @@ login_manager.login_register()  # open login/register page
 # load the data from the persistent storage into the session state
 data_manager.load_user_data(
     session_state_key='data_df', 
-    file_name='CO2_Fussabdruck/data.csv', 
+    file_name='data.csv', 
     initial_value = pd.DataFrame(), 
     parse_dates = ['timestamp']
 )
