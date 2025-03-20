dceac37c43c5985f27a0dafa086875e10b18ffcf
Albina <krasnal2@students.zhaw.ch>
2025-03-20 09:48:47 +0100

    co2 emission in grafik entfernt

--- BMLD-Inf2-AppV0-main/pages/CO2 Fussabdruck Grafik.py
+++ BMLD-Inf2-AppV0-main/pages/CO2 Fussabdruck Grafik.py
@@ -6,7 +6,7 @@ st.title("🌍 CO₂-Fussabdruck Verlauf")
 
 # Sicherstellen, dass die Daten im Session State existieren
 if 'data_df' not in st.session_state:
-    st.session_state['data_df'] = pd.DataFrame(columns=['co2_emission'])
+    st.session_state['data_df'] = pd.DataFrame(columns=["Transportmittel", "Kilometer pro Tag", "CO₂ (kg/Jahr)"])
 
 data_df = st.session_state['data_df']
 if data_df.empty:
@@ -14,5 +14,5 @@ if data_df.empty:
     st.stop()
 
 # Verlauf des CO₂-Ausstoßes anzeigen
-st.line_chart(data_df['co2_emission'], use_container_width=True)
+st.line_chart(data_df, use_container_width=True)
 st.caption('Jährlicher CO₂-Ausstoss über Zeit (kg)')
