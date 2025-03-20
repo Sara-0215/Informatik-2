28bbafee8cbce02b9aa737d1f67804c1b167c20c
Albina <krasnal2@students.zhaw.ch>
2025-03-20 08:21:50 +0100

    timestap entfernt

--- BMLD-Inf2-AppV0-main/Start.py
+++ BMLD-Inf2-AppV0-main/Start.py
@@ -18,8 +18,7 @@ login_manager.login_register()
 data_manager.load_user_data(
     session_state_key='data_df', 
     file_name='data.csv', 
-    initial_value = pd.DataFrame(columns=["Transportmittel", "Kilometer pro Tag", "Jährlicher CO₂-Ausstoss (kg)", "timestamp"]),
-    parse_dates=["timestamp"]
+    initial_value = pd.DataFrame(columns=["Transportmittel", "Kilometer pro Tag", "Jährlicher CO₂-Ausstoss (kg)",]),
 )
 #from utils.data_manager import DataManager
 
